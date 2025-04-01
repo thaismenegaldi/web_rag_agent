b9b1756af7a94bbec7b3640e1195eb264d705644
cayena <thaismenegaldi@ufmg.br>
2025-04-01 16:00:06 -0300

    feat: add vector retriever to load pdf files feat: initial project structure

---
+++ retrievers/vector_retriever.py
@@ -0,0 +1,41 @@
+from glob import glob
+from tqdm import tqdm
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+
+class VectorRetriever:
+    def __init__(self, path_to_data):
+        self.path_to_data = path_to_data
+
+    def load_data(self):
+        progress_bar = tqdm(
+            total=len(glob(self.path_to_data)), desc="Loading files..."
+        )
+        documents = []
+
+        for data_path in glob(self.path_to_data):
+            loader = PyPDFLoader(data_path)
+            documents.extend(loader.load())
+
+            progress_bar.update(1)
+
+        progress_bar.close()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500, chunk_overlap=50
+        )
+        texts = text_splitter.split_documents(documents)
+
+        embeddings = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-mpnet-base-v2"
+        )
+
+        vectorstore = FAISS.from_documents(texts, embeddings)
+        vectorstore.save_local("faiss_index")
+
+        retriever = vectorstore.as_retriever()
+
+        return retriever
