b9dd8eeaf0ea6f3bb426973e92d9cae01c903507
cayena <thaismenegaldi@ufmg.br>
2025-04-01 16:07:29 -0300

    feat: add base class Prompt to standardize the prompts definitions feat: add question and answering prompt

---
+++ prompts/prompt.py
@@ -0,0 +1,12 @@
+from typing import Dict, Literal
+from pydantic import BaseModel
+
+
+class Prompt(BaseModel):
+    role: Literal["system", "user"]
+    name: str
+    prompt_template: str
+
+    def format(self, fields: Dict[str, str] | None) -> str:
+        fields = fields or {}
+        return self.prompt_template.format(**fields)
---
+++ prompts/question_answering_prompt.py
@@ -0,0 +1,25 @@
+from prompt import Prompt
+
+QEA_SYSTEM_PROMPT = Prompt(
+    role="system",
+    name="question_answering_system",
+    prompt_template=(
+        """
+        You are an assistant for question-answering tasks.
+        Use the following pieces of retrieved context to answer the question.
+        If you don't know the answer, just say that you don't know.
+        """
+    )
+)
+
+QEA_USER_PROMPT = Prompt(
+    role="user",
+    name="question_answering_user",
+    prompt_template=(
+        """
+        User's question: {question}
+
+        Context: {context}
+        """
+    )
+)
