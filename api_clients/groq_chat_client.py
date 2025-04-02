e14302ea5688bae0b5a3a1c86f838c43383de28a
cayena <thaismenegaldi@ufmg.br>
2025-04-02 18:15:57 -0300

    feat: add generate_structured_response method to return structured outputs as json style: code formatting

--- api_clients/groq_chat_client.py
+++ api_clients/groq_chat_client.py
@@ -1,11 +1,18 @@
 import os
+from pathlib import Path
+from typing import Type, TypeVar
+
+import groq
+import instructor
 import yaml
 from dotenv import load_dotenv
-import groq
-from pathlib import Path
+from groq import Groq
+from pydantic import BaseModel
 
 # take environment variables
 load_dotenv()
+
+T = TypeVar("T", bound=BaseModel)
 
 
 class GroqChatClient:
@@ -15,6 +22,10 @@ class GroqChatClient:
             raise ValueError("GROQ_API_KEY environment variable is missing.")
 
         self.groq_client = groq.Client(api_key=api_key)
+        self.groq_instructor = instructor.from_groq(
+            Groq(), mode=instructor.Mode.JSON
+        )
+
         self.config = self._load_yaml_config(config_path)
 
     @staticmethod
@@ -23,19 +34,33 @@ class GroqChatClient:
             config = yaml.safe_load(file)
             return config
 
-    def generate_response(self, system_message: str, user_message: str):
+    def generate_response(self, system_message: str, user_message: str) -> str:
         try:
             response = self.groq_client.chat.completions.create(
                 model=self.config["llm"]["model"],
-                messages=[
-                    system_message,
-                    user_message
-                ],
+                messages=[system_message, user_message],
                 temperature=self.config["llm"]["temperature"],
                 top_p=self.config["llm"]["top_p"],
-                max_tokens=self.config["llm"]["max_tokens"]
+                max_tokens=self.config["llm"]["max_tokens"],
             )
 
             return response.choices[0].message.content
+        except Exception as e:
+            raise RuntimeError(f"Failed to generate response: {e}")
+
+    def generate_structured_response(
+        self, system_message: str, user_message: str, response_model: Type[T]
+    ) -> T:
+        try:
+            response = self.groq_instructor.chat.completions.create(
+                model=self.config["llm"]["model"],
+                messages=[system_message, user_message],
+                temperature=self.config["llm"]["temperature"],
+                top_p=self.config["llm"]["top_p"],
+                max_tokens=self.config["llm"]["max_tokens"],
+                response_model=response_model,
+            )
+
+            return response
         except Exception as e:
             raise RuntimeError(f"Failed to generate response: {e}")
