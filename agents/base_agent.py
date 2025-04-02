bdfab225aed2b1d9c9aa8cbdbfc5a877abe29b89
cayena <thaismenegaldi@ufmg.br>
2025-04-02 20:22:11 -0300

    feat: create a base agent to use a common base class for the agents

---
+++ agents/base_agent.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Dict
+
+from ..api_clients.groq_chat_client import GroqChatClient
+
+
+class BaseAgent(ABC):
+    def __init__(self, config_path: Path) -> None:
+        self.config_path = config_path
+        self.chat_client = GroqChatClient(config_path=config_path)
+
+    @abstractmethod
+    def get_system_prompt(self) -> Dict[str, str]:
+        pass
+
+    @abstractmethod
+    def get_user_prompt(self, **kwargs) -> Dict[str, str]:
+        pass
+
+    @abstractmethod
+    def generate_response(self, **kwargs):
+        pass
