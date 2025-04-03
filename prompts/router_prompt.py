9c33c10b9d3c67d03e2ba8f4b48e92390f618ae9
cayena <thaismenegaldi@ufmg.br>
2025-04-03 10:24:01 -0300

    feat: add router agent prompts

---
+++ prompts/router_prompt.py
@@ -0,0 +1,43 @@
+from prompt import Prompt
+
+ROUTER_SYSTEM_PROMPT = Prompt(
+    role="system",
+    name="router_system",
+    prompt_template=(
+        """
+        You are an expert at routing user questions to either a vectorstore or
+        web search based on their topic.
+            - Use "vectorstore" for questions related to  Harry Potter, the
+            wizarding world, magical creatures, spells, characters,
+            or similar themes.
+            - Use "web_search" for all other topics.
+
+        Output your decision as a JSON object with a single key "datasource"
+        and no additional explanation.
+
+        Examples:
+        Question: Who is Hermione Granger?
+        Answer: {{"datasource": "web_search"}}
+
+        Question: What is the Accio spell used for??
+        Answer: {{"datasource": "vectorstore"}}
+
+        Question: What are the La Liga next fixtures?
+        Answer: {{"datasource": "vectorstore"}}
+
+        Question: What is prompt engineering?
+        Answer: {{"datasource": "vectorstore"}}
+        """
+    )
+)
+
+ROUTER_USER_PROMPT = Prompt(
+    role="user",
+    name="router_user",
+    prompt_template=(
+        """
+        Question to route:
+        {question}
+        """
+    )
+)
