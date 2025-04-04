adc927cbd5c2e0b063cd59389befa2f26eec0ccb
cayena <thaismenegaldi@ufmg.br>
2025-04-04 10:10:00 -0300

    feat: add class to make requests to serpapi api

---
+++ api_clients/serp_api_client.py
@@ -0,0 +1,25 @@
+import os
+from typing import Dict
+
+from dotenv import load_dotenv
+import requests
+
+# take environment variables
+load_dotenv()
+
+
+class SerpAPIClient:
+    def __init__(self):
+        self.api_key = os.getenv("SERPAPI_KEY")
+        if not self.api_key:
+            raise ValueError("SERPAPI_KEY environment variable is missing.")
+
+    def search_tool(self, query: str) -> Dict:
+
+        # general google search
+        url = f"https://serpapi.com/search.json?q={query}&hl=en&api_key={self.api_key}"  # noqa: E501
+
+        # get response
+        response = requests.get(url).json()
+
+        return response
