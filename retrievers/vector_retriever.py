b006f70de2e3db3e659da295d0131c78e9bd2865
Thais Menegaldi <thaismenegaldi@ufmg.br>
2025-04-05 20:13:03 -0300

    feat: load local vector store when it exists

--- retrievers/vector_retriever.py
+++ retrievers/vector_retriever.py
@@ -1,12 +1,18 @@
-from glob import glob
 from tqdm import tqdm
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from ..utils.load_config import load_yaml_config
-from langchain_core.vectorstores.base import VectorStoreRetriever
+from utils.load_config import load_yaml_config
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="\n%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 
 class VectorRetriever:
@@ -14,35 +20,51 @@ class VectorRetriever:
         self.path_to_data = path_to_data
         self.config = load_yaml_config(config_path)
 
-    def load_data(self) -> VectorStoreRetriever:
-        files = glob(f"{self.path_to_data}/*.pdf")
+    def load_data(self):
+        data_path = Path(self.path_to_data)
+        entries = [p.name for p in data_path.iterdir()]
+        pdf_files = list(data_path.glob("*.pdf"))
 
-        progress_bar = tqdm(
-            total=len(files), desc="Loading files..."
-        )
-        documents = []
+        if "faiss_index" not in entries:
+            progress_bar = tqdm(
+                total=len(pdf_files), desc="Loading files..."
+            )
+            documents = []
 
-        for data_path in files:
-            loader = PyPDFLoader(data_path)
-            documents.extend(loader.load())
+            for data_path in pdf_files:
+                loader = PyPDFLoader(data_path)
+                documents.extend(loader.load())
 
-            progress_bar.update(1)
+                progress_bar.update(1)
 
-        progress_bar.close()
+            progress_bar.close()
 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=self.config["retriever"]["chunk_size"],
-            chunk_overlap=self.config["retriever"]["chunk_overlap"]
-        )
-        texts = text_splitter.split_documents(documents)
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=self.config["retriever"]["chunk_size"],
+                chunk_overlap=self.config["retriever"]["chunk_overlap"]
+            )
+            texts = text_splitter.split_documents(documents)
 
-        embeddings = HuggingFaceEmbeddings(
-            model_name=self.config["retriever"]["model"],
-        )
+            embeddings = HuggingFaceEmbeddings(
+                model_name=self.config["retriever"]["model"],
+            )
 
-        vectorstore = FAISS.from_documents(texts, embeddings)
-        vectorstore.save_local("faiss_index")
+            vector_store = FAISS.from_documents(texts, embeddings)
+            vector_store.save_local("faiss_index")
 
-        retriever = vectorstore.as_retriever()
+        else:
+            logging.info("Loading local vector store...")
+            embeddings = HuggingFaceEmbeddings(
+                model_name=self.config["retriever"]["model"],
+            )
+
+            vector_store = FAISS.load_local(
+                "faiss_index",
+                embeddings,
+                allow_dangerous_deserialization=True
+            )
+
+
+        retriever = vector_store.as_retriever()
 
         return retriever
